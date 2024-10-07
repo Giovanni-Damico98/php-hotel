5c27b917c88b8452c745ae8283186c8dc0148db1
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-10-07 14:08:29 +0200

    html + php progress

--- index.php
+++ index.php
@@ -1,3 +1,93 @@
-<?php 
+<?php
+$hotels = [
 
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+</head>
+
+<body>
+    <div class="container">
+
+        <table class="table table-striped">
+            <!-- Voci della tabella -->
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrizione</th>
+                    <th>Parcheggio</th>
+                    <th>Voto</th>
+                    <th>Distanza dal centro</th>
+                </tr>
+            </thead>
+            <!-- Corpo della tabella -->
+            <tbody>
+                <!-- Per ogni hotel nell'array hotel eseguo un ciclo che mi permetta di vedere ogni campo -->
+                <?php foreach ($hotels as $hotel) { ?>
+                    <!-- Table row -->
+                    <tr>
+                        <!-- Table Data + codice php per mostrare in pagina i campi -->
+                        <td><?php
+                            echo $hotel['name']; ?></td>
+                        <td><?php
+                            echo $hotel['description']; ?></td>
+                        <td><?php
+                            echo $hotel['parking'] ? 'Si' : 'No'; ?></td>
+                        <td><?php
+                            echo $hotel['vote']; ?></td>
+                        <td><?php
+                            echo $hotel['distance_to_center']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+    </div>
+    </table>
+</body>
+
+</html>
